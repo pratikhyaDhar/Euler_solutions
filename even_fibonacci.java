efbd0750bb7b38ed77d2738a91748cd9a28cab13
Pratikhya <92398878+Satorusimpp@users.noreply.github.com>
2022-12-08 00:10:28 +0530

    Update even_fibonacci.java

--- even_fibonacci.java
+++ even_fibonacci.java
@@ -4,7 +4,7 @@ public class even_fibonacci
  {
     public static void main(String args[]) {
     int a=0,b=1,c,s=0;    		    
-		 for(int i=2;i<1000000;i++)   
+		 for(int i=2;i<4000000;i++)   
 		 {    
 		  c=a+b;    
 		  if(c%2==0) {
